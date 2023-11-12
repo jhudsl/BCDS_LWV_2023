90c0ca7833c4256b35c03bb6ab006b1101ad9449
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-11-11 19:22:05 -0500

    fix "WRITE-IN" mistake
    remove false matches; "WRITE-IN" is a candidate for every office being voted on
    also, extract candidate party and parse US senator results

--- code/wrangle_2022_general_election_results.R
+++ code/wrangle_2022_general_election_results.R
@@ -13,10 +13,12 @@ all_precinct_results <- extract_text(paste0(dir, "data/input/public/baltimore_ci
 
 # get names of precincts (three digits before the hyphen indicate the ward)
 regex_for_precincts <- "\\d{3}-\\d{3}"
-precinct_names <- unique(unlist(str_extract_all(all_precinct_results, regex_for_precincts)))
+precinct_names <- unique(unlist(str_extract_all(string = all_precinct_results,
+                                                pattern = regex_for_precincts)))
 
 # parse election results
-results_line_by_line <- unlist(str_split(all_precinct_results, "\n"))
+results_line_by_line <- unlist(str_split(string = all_precinct_results,
+                                         pattern = "\n"))
 
 turnout_variables <- c("REGISTERED VOTERS - TOTAL",
                        "BALLOTS CAST - TOTAL",
@@ -27,44 +29,78 @@ governor_candidates <- c("Cox-Schifanelli",
                          "Wallace-Elder",
                          "Harding-White",
                          "WRITE-IN")
+us_senator_candidates <- c("Chris Chaffee",
+                           "Chris Van Hollen",
+                           "WRITE-IN")
 
 parse_turnout_results <- function(variable){
-  results_as_text <- results_line_by_line[grepl(variable, results_line_by_line)]
+  results_as_text <- str_extract(string = results_line_by_line,
+                                 pattern = paste0(variable, ".+?\\d+"))
+  results_as_text <- results_as_text[!is.na(results_as_text)]
   
-  results_as_tibble <- tibble(temp = results_as_text) %>%
+  results_as_tibble <- tibble(text = results_as_text) %>%
     mutate(Precinct = precinct_names,
-           Variable = str_extract(temp, "[A-z\\- ]+"),
-           Count = str_extract(temp, "\\d+"),
-           temp = NULL)
+           Variable = variable,
+           Count = str_extract(text, "\\d+"),
+           text = NULL)
+  return(results_as_tibble)
 }
 
-parse_candidate_results <- function(candidate, election_number_in_report){
-  results_as_text <- results_line_by_line[grepl(candidate, results_line_by_line)]
-  
-  if (candidate == "WRITE-IN"){ # "WRITE-IN" is an option in every government office being voted on, so filter to the correct one
-    election_indices <- rep(FALSE, 14) # there are 14 elections (aka races aka government offices being voted on) in this report
-    election_indices[election_number_in_report] <- TRUE
-    results_as_text <- results_as_text[election_indices] # keep 1 in every 14 "WRITE-IN" regex matches
+parse_candidate_results <- function(candidate, all_candidates){
+  if (candidate == "WRITE-IN"){
+    # every election (office being voted on) has "WRITE-IN" as a candidate, so need extra filtering
+    all_write_in_indices <- grep(pattern = "WRITE-IN",
+                                 x = results_line_by_line)
+    
+    # trick: to identify which WRITE-IN we want, use the last candidate in the election (office being voted on) we want
+    last_candidate <- all_candidates[length(all_candidates) - 1]
+    last_candidate_indices <- grep(pattern = last_candidate,
+                                   x = results_line_by_line)
+    write_in_indices <- last_candidate_indices + 1 # WRITE-IN should be in the line after the last candidate
+    
+    # except, if there is a page break, then WRITE-IN will be 3 lines instead of 1 line after the last candidate
+    for (i in 1:length(write_in_indices)){
+      if (!(write_in_indices[i] %in% all_write_in_indices) &
+          (write_in_indices[i] + 2 %in% all_write_in_indices)){ # +2 if new page
+        write_in_indices[i] <- write_in_indices[i] + 2
+      }
+    }
+    
+    # now we can pull the text!
+    results_as_text <- str_extract(string = results_line_by_line[write_in_indices],
+                                   pattern = paste0("WRITE-IN", ".+?\\d+"))
+  } else{
+    # for registered candidates, we can do the usual str_extract() call
+    results_as_text <- str_extract(string = results_line_by_line,
+                                   pattern = paste0(candidate, ".+?\\d+"))
+    results_as_text <- results_as_text[!is.na(results_as_text)]
+    
   }
   
-  results_as_tibble <- tibble(temp = results_as_text) %>%
+  # parse and put the results in a table
+  results_as_tibble <- tibble(text = results_as_text) %>%
     mutate(Precinct = precinct_names,
-           Candidate = str_extract(temp, "[A-z\\- ()]+"), # "()" includes candidates' party affiliation, which is enclosed in parentheses
-           Votes = str_extract(temp, "\\d+"),
-           temp = NULL)
+           Candidate = candidate,
+           Affiliation = str_extract(text, "\\([A-z]+\\)"),
+           Votes = str_extract(text, "\\d+"),
+           text = NULL) %>%
+    mutate(Affiliation = str_remove_all(Affiliation, "\\(|\\)"))
+  return(results_as_tibble)
 }
 
-turnout_results <- lapply(turnout_variables, parse_turnout_results) %>%
+turnout_results <- lapply(turnout_variables,
+                          parse_turnout_results) %>%
   bind_rows()
-governor_results <- lapply(governor_candidates, parse_candidate_results, election_number_in_report = 1) %>%
+governor_results <- lapply(governor_candidates,
+                           parse_candidate_results,
+                           all_candidates = governor_candidates) %>%
   bind_rows()
-
-
-
-# preview the data
-head(turnout_results)
-head(governor_results)
+us_senator_results <- lapply(us_senator_candidates,
+                             parse_candidate_results,
+                             all_candidates = us_senator_candidates) %>%
+  bind_rows()
 
 # save the data
 write_csv(turnout_results, file = paste0(dir, "data/intermediate/public/baltimore_city_2022_general_election_turnout_results.csv"))
 write_csv(governor_results, file = paste0(dir, "data/intermediate/public/baltimore_city_2022_general_election_governor_results.csv"))
+write_csv(us_senator_results, file = paste0(dir, "data/intermediate/public/baltimore_city_2022_general_election_us_senator_results.csv"))
