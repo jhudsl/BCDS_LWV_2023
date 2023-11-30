b1669a783f5317e5a1fef0e849f789d2407b175f
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-11-30 14:30:32 -0500

    Read all of table 2

--- code/read_2020_precinct_population_data.R
+++ code/read_2020_precinct_population_data.R
@@ -4,85 +4,108 @@ library(tidyverse) # for piping (%>%) and various functions
 # location of this repository on user's computer
 dir <- "../"
 
-### Download 2020 precinct population counts corrected for prison gerrymandering (a PDF)
+##### Download 2020 precinct population counts corrected for prison gerrymandering (a PDF)
 
 ## Uncomment this line if data isn't downloaded yet
 
 # download.file(url = "https://planning.maryland.gov/Redistricting/Documents/2020data/GreenReport.pdf",
 #               destfile = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"))
 
-### Read population data, as a table from a PDF
+
+##### Read population data, as a table from a PDF
 
 ## Select Table 2 (adjusted total population)
 
-# first, get dimensions of page 53 (first page containing table 2)
+# first, get dimensions of Table 2 (pages 53-58)
 page_dims_table2 <- get_page_dims(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                                  pages = 53)
+                                  pages = 53:58)
 # page_dims_table2
 # [[1]]
 # [1] 1342.37 1037.29
+# 
+# [[2]]
+# [1] 1342.37 1037.29
+# 
+# [[3]]
+# [1] 1342.37 1037.29
+# 
+# [[4]]
+# [1] 1342.37 1037.29
+# 
+# [[5]]
+# [1] 1342.37 1037.29
+# 
+# [[6]]
+# [1] 1342.37 1037.29
 
 
-# parse page 53
-table2p1 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                           pages = 53,
-                           area = list(c(260, 1, 1030, 1342)), # c(top, left, bottom, right); eyeball from PDF and page dimensions; 260 is approx 1037.29/4
+# parse Table 2
+# eyeball "area" from PDF and page dimensions; 260 is approximately 1037.29/4
+table2 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
+                           pages = 53:58,
+                           area = list(c(260, 1, 1030, 1342)), # c(top, left, bottom, right)
                            guess = F)
 
-## to do: use 53:58 instead of 53?
-
 # clean the parsed data
-table2p1 <- table2p1[[1]] %>%
-  as_tibble %>%
+table2 <- table2 %>%
+  lapply(as_tibble) %>%
+  bind_rows() %>%
   filter(grepl(pattern = "Baltimore City Precinct", x = V1)) %>%
   select(-c(V1, V7, V16)) # remove blank nonexistent columns
-colnames(table2p1) <- c("Precinct", "Adjusted_or_Unadjusted", "Census_Pop", "Adjusted_Total_Pop",
+
+colnames(table2) <- c("Precinct", "Adjusted_or_Unadjusted", "Census_Pop", "Adjusted_Total_Pop",
                         "Adjusted_One_Race_Pop", "Adjusted_White_Alone_Pop", "Adjusted_Black_Alone_Pop",
                         "Adjusted_American_Indian_Alaskan_Native_Alone_Pop", "Adjusted_Asian_Alone_Pop",
                         "Adjusted_Native_Hawaiian_Pacific_Islander_Alone_Pop", "Adjusted_Other_Race_Alone_Pop",
                         "Adjusted_Multiracial_Pop", "Adjusted_Hispanic_Latino_Pop", "Adjusted_Total_18Plus_Pop")
 
 
-# table2p1 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-#                          pages = 53,
-#                          columns = list(20), # or list(17)
-#                          guess = F)
-
-table2 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                         pages = c(53:58),
-                         columns = list(20),
-                         guess = F) # set guess = FALSE because we can tell extract_tables() the number of columns
-
-
 ## Select Table 3 (adjusted voting-age population)
 
-table3 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                         pages = 88:92,
-                         columns = list(21),
-                         guess = F) # set guess = FALSE because we can tell extract_tables() the number of columns
-
-# first, get dimensions of page 88 (first page containing table 3)
+# first, get dimensions of Table 3 (pages 88-92)
 page_dims_table3 <- get_page_dims(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                                  pages = 88)
+                                  pages = 88:92)
 # page_dims_table3
 # [[1]]
 # [1] 1466.67 1133.33
+# 
+# [[2]]
+# [1] 1466.67 1133.33
+# 
+# [[3]]
+# [1] 1466.67 1133.33
+# 
+# [[4]]
+# [1] 1466.67 1133.33
+# 
+# [[5]]
+# [1] 1466.67 1133.33
 
-# parse page 88
-table3p1 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
-                           pages = 88,
-                           area = list(c(188, 1, 1030, 1466)), # c(top, left, bottom, right); eyeball from PDF and page dimensions; 188 is approx 1133.33/6
+# parse Table 3
+# eyeball "area" from PDF and page dimensions; 188 is approximately 1133.33/6
+table3 <- extract_tables(file = paste0(dir, "data/input/public/Maryland/precinct_population_data_2020.pdf"),
+                           pages = 88:92,
+                           area = list(c(188, 1, 1030, 1466)), # c(top, left, bottom, right)
                            guess = F)
 
+## to do: below this. out of memory Java heap space...
+
+# clean the parsed data
+table3 <- table3 %>%
+  lapply(as_tibble) %>%
+  bind_rows() %>%
+  filter(grepl(pattern = "Baltimore City Precinct", x = V1)) %>%
+  select(-c(V1, V7, V16)) # remove blank nonexistent columns
+
+colnames(table3) <- c("Precinct", "Adjusted_or_Unadjusted", "Census_Pop", "Adjusted_Total_Pop",
+                      "Adjusted_One_Race_Pop", "Adjusted_White_Alone_Pop", "Adjusted_Black_Alone_Pop",
+                      "Adjusted_American_Indian_Alaskan_Native_Alone_Pop", "Adjusted_Asian_Alone_Pop",
+                      "Adjusted_Native_Hawaiian_Pacific_Islander_Alone_Pop", "Adjusted_Other_Race_Alone_Pop",
+                      "Adjusted_Multiracial_Pop", "Adjusted_Hispanic_Latino_Pop", "Adjusted_Total_18Plus_Pop")
 
 
+##### Save the resulting data tables
 
-
-# consider using row_split(), col_paste()
-
-
-
-# save the resulting data tables
 # write_csv(table2, file = paste0(dir, "data/intermediate/public/Baltimore_City/adjusted_precinct_populations_2020.csv"))
 # write_csv(table3, file = paste0(dir, "data/intermediate/public/Baltimore_City/adjusted_voting_age_populations_2020.csv"))
 
