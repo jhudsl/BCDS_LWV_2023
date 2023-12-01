b011077ac96b5c1efe984b2a111dfc0af940254b
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2023-11-30 22:30:53 -0500

    Clarify votes = votes for in election results csv

--- code/read_2022_general_election_results_from_csv_including_ballot_type.R
+++ code/read_2022_general_election_results_from_csv_including_ballot_type.R
@@ -10,10 +10,33 @@ dir <- "../"
 # read all election results, as csv
 all_precinct_results <- read_csv(file = paste0(dir, "data/input/public/Baltimore_City/general_election_2022/precinct_results_including_ballot_type.csv"))
 
-# create new column(s)
-all_precinct_results <- all_precinct_results %>% mutate(`Total Votes` = `Early Votes` + `Election Night Votes` +
-                                                          `Mail-In Ballot 1 Votes` + `Mail-In Ballot 2 Votes` +
-                                                          `Provisional Votes`)
+# NOTE: in the raw dataset, "Votes" means "Votes For"
+# rename columns to be clearer
+all_precinct_results <- all_precinct_results %>%
+  rename(`Early Votes for Candidate` = `Early Votes`,
+         `Election Night Votes for Candidate` = `Election Night Votes`,
+         `Mail-In Ballot 1 Votes for Candidate` = `Mail-In Ballot 1 Votes`,
+         `Mail-In Ballot 2 Votes for Candidate` = `Mail-In Ballot 2 Votes`,
+         `Provisional Votes for Candidate` = `Provisional Votes`)
+
+# create new columns
+all_precinct_results <- all_precinct_results %>%
+  rowwise() %>%
+  mutate(`Total Votes For Candidate` = sum(`Early Votes for Candidate`,
+                                           `Election Night Votes for Candidate`,
+                                           `Mail-In Ballot 1 Votes for Candidate`,
+                                           `Mail-In Ballot 2 Votes for Candidate`,
+                                           `Provisional Votes for Candidate`,
+                                           na.rm = T),
+         `Total Votes Against` = sum(`Early Votes Against`,
+                                     `Election Night Votes Against`,
+                                     `Mail-In Ballot 1 Votes Against`,
+                                     `Mail-In Ballot 2 Votes Against`,
+                                     `Provisional Votes Against`,
+                                     na.rm = T),
+         `Total Votes Cast` = sum(`Total Votes For Candidate`,
+                                  `Total Votes Against`,
+                                  na.rm = T))
 
 # save the resulting data table(s)
 write_csv(all_precinct_results, file = paste0(dir, "data/intermediate/public/Baltimore_City/general_election_2022/all_election_results_by_ballot_type.csv"))
